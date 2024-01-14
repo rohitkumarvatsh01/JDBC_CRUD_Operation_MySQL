8f66825606183e261e77e02a9b8c9d8e03da290b
Rohit Kumar <85385695+rohitkumarvatsh01@users.noreply.github.com>
2024-01-14 22:15:19 +0530

    Add files via upload

---
+++ CreateJDBC.java
@@ -0,0 +1,28 @@
+package Jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CreateJDBC {
+	public static void main(String[] args) {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/College","root","Rohitkumar01@");
+			Statement stmt=con.createStatement();
+			
+			System.out.println("Inserting the Value in Database");
+			
+			String query="Insert into Student values(10, 'Rohan', 'Bihar', 70, 'D')";
+			stmt.execute(query);
+			
+			con.close();
+			stmt.close();
+			
+		}
+		catch(Exception e) {
+			System.out.println("Connection is Not Successfully "+e);
+		}
+	}
+}
