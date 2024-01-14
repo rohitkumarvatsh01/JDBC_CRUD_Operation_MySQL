bb883bd3e50737ac8b4ccc0033c26d70fd674130
Rohit Kumar <85385695+rohitkumarvatsh01@users.noreply.github.com>
2024-01-14 23:15:58 +0530

    UpdateJDBC.java

--- UpdateJDBC.java
+++ UpdateJDBC.java
@@ -11,27 +11,27 @@ public class UpdateJDBC {
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/College","root","root");
+			
+			String urlDB="jdbc:mysql://localhost:3306/College";
+			String userName="root";
+			String password="root";
+			Connection con=DriverManager.getConnection(urlDB, userName, password);
 			
 			String query="UPDATE Student SET Name=?, City=? WHERE RollNo=?";
 			
 			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-			
+		
 			System.out.println("Enter the New Name:- ");
 			String name=br.readLine();
-			
 			System.out.println("Enter the New City Name:- ");
 			String city=br.readLine();
-			
 			System.out.println("Enter  the RollNo:- ");
 			int roll=Integer.parseInt(br.readLine());
 			
 			PreparedStatement pstmt=con.prepareStatement(query);
-			
 			pstmt.setString(1, name);
 			pstmt.setString(2, city);
 			pstmt.setInt(3, roll);
-			
 			pstmt.executeUpdate();
 			
 			System.out.println("Update is Successfully");
